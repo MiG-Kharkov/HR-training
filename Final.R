59d4d09264af0c06284b870573fe94fd55e6b063
Natasha <manzhos.natasha@gmail.com>
2017-01-19 20:46:39 +0200

    Creat CAP curve

--- Final.R
+++ Final.R
@@ -9,7 +9,8 @@ if (!require("gridExtra"))    install.packages("gridExtra")
 if (!require("ROCR"))    install.packages("ROCR")
 if (!require("fBasics"))    install.packages("fBasics")
 if (!require("randomForest"))    install.packages("randomForest")
-
+if (!require("vcd"))    install.packages("vcd")
+if (!require("LDPD"))    install.packages("LDPD")
 
 # Load librarys
 library(caret)
@@ -23,13 +24,15 @@ library(ROCR)
 library(fBasics)
 library(randomForest)
 library(ElemStatLearn)
-
+library(vcd)
+library(LDPD)
 
 # Read database
 dataset <- read.csv("HR_comma_sep.csv")
 
 # look at data
 sapply(dataset, function(x) length(unique(x)))
+#how many unique elements we have. What variable are or can be seen as factors
 str(dataset)
 summary(dataset)
 
@@ -50,7 +53,7 @@ summary(dataset)
 # $ sales                : Factor w/ 10 levels "accounting","hr",..: 8 8 8 8 8 8 8 8 8 8 ...
 # $ salary               : Factor w/ 3 levels "high","low","medium": 2 3 3 2 2 2 2 2 2 2 ...
 
-#Change some variables as factors
+#Change some variables type at factors
 
 dataset$left <- as.factor(dataset$left)
 dataset$promotion_last_5years<- as.factor(dataset$promotion_last_5years)
@@ -68,7 +71,7 @@ hist(dataset$average_montly_hours, col="lightyellow",  freq = FALSE, main = past
 hist(dataset$number_project, col="lightyellow",breaks = 5, freq = FALSE, main = paste('Number of Projects'), xlab = "x")
 hist(dataset$time_spend_company, col="lightyellow",  freq = FALSE, main = paste('Time at company'), xlab = "x")
 
-
+#density for different factors
 g1<-ggplot(dataset, aes(x = last_evaluation, colour = factor(left))) + 
   geom_density() + ggtitle("Last Evaluation")
 g2<-ggplot(dataset, aes(x = last_evaluation, colour = factor(salary))) + 
@@ -87,19 +90,17 @@ g2<-ggplot(dataset, aes(x =  average_montly_hours, colour = factor(salary))) +
   geom_density() + ggtitle(" ")
 grid.arrange(g1,g2, nrow=2, ncol=1)
 
-
-             
-#The density for variables are different for different left factors, and the same for different salary factors.  
+#The graphs of density for variables differ for different left factors, but have almost the same type for different salary factors.  
 #From graphs none of these variables are normal. We can also check null hypothesis  that the population is normally distributed
-#with such tests as ad.test(), shapiro.test(),lillie.test() and others
+#with such tests as ad.test(), shapiro.test(),lillie.test() or others
 
 set.seed(123)
 split = createDataPartition(y=dataset$left, p=0.33, list=FALSE)
 smallsample <- dataset[split, ]
 
-print(shapiroTest(smallsample$last_evaluation))
-print(ksnormTest(unique(dataset$last_evaluation)))
-print(adTest(sort(dataset$last_evaluation)))
+print(shapiroTest(smallsample$last_evaluation)) # woks for size of sample <=5000
+print(ksnormTest(unique(dataset$last_evaluation)))# woks only for unique elements
+print(adTest(dataset$last_evaluation))
 print(lillieTest(dataset$last_evaluation))
 
 # Shapiro - Wilk Normality Test
@@ -133,13 +134,13 @@ print(lillieTest(dataset$last_evaluation))
 # P VALUE:
 #   < 2.2e-16 
 
-# For other variables we have the same result
+# For other variables we have the same result: null hypotesis is rejected 
 
 # analysis of dependencies that affect leaves. 
 g1<-ggplot(dataset, aes(x =average_montly_hours, y =  time_spend_company))+ 
   geom_point(color = as.numeric(dataset$left))+
   geom_density2d()+
-  labs(title="The probability destribution of leaving", x = "Avrenge hours per month", y = "Years in the company")
+  labs(title="The probability destribution of leaving \n (red points show who left)", x = "Avrenge hours per month", y = "Years in the company")
 
 g2<-ggplot(dataset, aes(x =last_evaluation, y =  satisfaction_level))+ 
   geom_point(color = as.numeric(dataset$left))+
@@ -151,7 +152,7 @@ g2<-ggplot(dataset, aes(x =last_evaluation, y =  satisfaction_level))+
 grid.arrange(g1,g2, nrow=2, ncol=1)
 
 
-# check correlation and multicollinearity for numeric variables 
+# check correlation for numeric variables 
 
 par(mfrow=c(1,1))
 
@@ -165,11 +166,11 @@ corrplot(cor.data, method = "pie")
 cor.test(dataset$last_evaluation,dataset$number_project)
 cor.test(dataset$last_evaluation,dataset$average_montly_hours)
 cor.test(dataset$number_project,dataset$average_montly_hours)
+# we remember that coefficient tells us only about linear dependence. And between these variables there are not linear dependence.
+# but could be nonlinear
 
-vif(glm(formula = left ~ . ,family = binomial,data = dataset))
-#GVIFs are from 1 to 2 so there is no multicollinearity
-
-#MODEL 
+#Predicshion who are going to leave
+#Split data training : testing like 3:1
 set.seed(123)
 split = createDataPartition(y=dataset$left, p=0.75, list=FALSE)
 training <- dataset[split, ]
@@ -177,6 +178,7 @@ testing <- dataset[-split,]
 
 
 #MODEL 1 NAIVE BAYES
+set.seed(123)
 modelFit <- naiveBayes(left ~. , data = training )
 prediction_prob <- predict(modelFit,  newdata = testing[-7], type = "raw")
 prediction_bayes <- prediction_prob[,2]
@@ -212,21 +214,46 @@ confusionMatrix(y_hat, testing$left)
 # Sensitivity : 0.9188   means that we predict correct result 92% of stayed people      
 # Specificity : 0.5908 means that we predict correct result 59% of left people
 
+#Build CAP curve
 
-#ROC curve
+cap_data_bayes <- cbind(left = as.numeric(testing$left)-1, predicted = round(prediction_bayes,5))
+cap_data_bayes <- as.data.frame(cap_data_bayes)
+
+
+attach(cap_data_bayes)
+nraws <- length(left)
+nleft <- length(left[left==1])
+cap_data_bayes <- cbind (cap_data_bayes[order( -predicted),], Percent_of_Sample = (1:nraws)/nraws)
+n <-0
+for (i in 1:nraws) {
+  n <- cap_data_bayes[i,1]+n
+  cap_data_bayes[i,4]<- n/nleft
+}
+perc=nleft/nraws
+detach(cap_data_bayes)
+
+plot(x = cap_data_bayes[,3], y = cap_data_bayes[,4], col = "red", lwd =0.3, 
+     xlab = "Percent of Sample", ylab = "Predicted Left")
+lines(c(0,1),c(0,1), col = "gray", lwd =2)
+text(0.6,0.2,paste("Percent people left is ", round(perc,2)*100, sep="", "%" ), cex=1.4)
+title("CAP Curve")
+
+#Build ROC curve (it shows )
 
 par(mfrow=c(1,1))
 ROCRpred = prediction(prediction_bayes, testing$left)
 ROCRperf = performance(ROCRpred, "tpr", "fpr")
-auc <- slot(performance(ROCRpred, "auc"), "y.values")[[1]]
+auc <- slot(performance(ROCRpred, "auc"), "y.values")[[1]] # Area Under Curve
 plot(ROCRperf, colorize=TRUE)
 abline(h=seq(0,1,0.05), v=seq(0,1,0.05), col = "lightgray", lty = "dotted")
 lines(c(0,1),c(0,1), col = "gray", lwd =2)
 text(0.6,0.2,paste("AUC=", round(auc,4), sep=""), cex=1.4)
 title("ROC Curve Bayes")
 
-#MODEL 2 train with random forest model (packege Random Forest)
 
+
+#MODEL 2 train with random forest model (packege Random Forest) 
+set.seed(123)
 rf.model <- randomForest(x = training[-7], y = training$left)
 rf.predict <- predict(rf.model, testing[-7])
 confusionMatrix(rf.predict, testing$left)
@@ -234,8 +261,8 @@ confusionMatrix(rf.predict, testing$left)
 # 
 # Reference
 # Prediction    0    1
-# 0 2852   29
-# 1    5  863
+#           0 2852   29
+#           1    5  863
 # 
 # Accuracy : 0.9909          
 # 95% CI : (0.9873, 0.9937)
@@ -253,63 +280,4 @@ confusionMatrix(rf.predict, testing$left)
 #          Detection Rate : 0.7607          
 #    Detection Prevalence : 0.7685          
 #       Balanced Accuracy : 0.9829
-
-#MODEL 2 train with random forest model (packege caret)
-rf.model <- train(left ~., data = training, method = "rf")
-summary(rf.model)
-
-# call                4  -none-     call     
-# type                1  -none-     character
-# predicted       11250  factor     numeric  
-# err.rate         1500  -none-     numeric  
-# confusion           6  -none-     numeric  
-# votes           22500  matrix     numeric  
-# oob.times       11250  -none-     numeric  
-# classes             2  -none-     character
-# importance         18  -none-     numeric  
-# importanceSD        0  -none-     NULL     
-# localImportance     0  -none-     NULL     
-# proximity           0  -none-     NULL     
-# ntree               1  -none-     numeric  
-# mtry                1  -none-     numeric  
-# forest             14  -none-     list     
-# y               11250  factor     numeric  
-# test                0  -none-     NULL     
-# inbag               0  -none-     NULL     
-# xNames             18  -none-     character
-# problemType         1  -none-     character
-# tuneValue           1  data.frame list     
-# obsLevels           2  -none-     character
-
-rf.prediction <- predict(rf.model, newdata = testing[-7])
-confusionMatrix(rf.prediction, testing$left)
-
-# Confusion Matrix and Statistics
-# 
-# Reference
-# Prediction    0    1
-# 0 2852   28
-# 1    5  864
-# 
-# Accuracy : 0.9912          
-# 95% CI : (0.9877, 0.9939)
-# No Information Rate : 0.7621          
-# P-Value [Acc > NIR] : < 2.2e-16       
-# 
-# Kappa : 0.9755          
-# Mcnemar's Test P-Value : 0.0001283       
-#                                           
-#             Sensitivity : 0.9982          
-#             Specificity : 0.9686          
-#          Pos Pred Value : 0.9903          
-#          Neg Pred Value : 0.9942          
-#              Prevalence : 0.7621          
-#          Detection Rate : 0.7607          
-#    Detection Prevalence : 0.7682          
-#       Balanced Accuracy : 0.9834          
-#                                           
-#        'Positive' Class : 0
-
-
-
 
